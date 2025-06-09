d306246500862f7954efb108080b6c110ba24319
Christos Kostanikos <kostanikos10@gmail.com>
2025-06-09 14:24:38 +0300

    Add Navbar component for navigation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
-import { Button, Container, HStack } from "@chakra-ui/react";
+import { Button, Container, HStack, Flex, Text, Box } from "@chakra-ui/react";
+import { useColorMode } from "@/components/ui/color-mode";
+import { Icon } from "@chakra-ui/react";
+import { LuSquarePlus, LuSun } from "react-icons/lu";
+
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoMoon } from "react-icons/io5";
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
@@ -15,19 +22,28 @@ const Navbar = () => {
         }}
       >
         <Text
-          fontSize={{ base: "22", sm: "28" }}
-          fontWeight={"bold"}
-          textTransform={"uppercase"}
-          textAlign={"center"}
-          bgGradient={"linear(to-r, cyan.400, blue.500)"}
-          bgClip={"text"}
+          fontSize={{ base: "22px", sm: "28px" }}
+          fontWeight="bold"
+          textTransform="uppercase"
+          textAlign="center"
+          //bgGradient="linear(to-r,#fff, #90cdf4, #3182ce, #1a365d)"
+          //bgClip="text"
+          color={"cadetblue"}
         >
-          <Link to={"/"}>Product Store </Link>
+          <Link to="/">Product Store 🛒 </Link>
         </Text>
+
         <HStack spacing={2} alignItems={"center"}>
           <Link to={"/create"}>
-            <Button></Button>
+            <Button variant={"surface"} size="lg">
+              <Icon>
+                <LuSquarePlus />
+              </Icon>
+            </Button>
           </Link>
+          <Button variant={"surface"} size="lg" onClick={toggleColorMode}>
+            {colorMode === "light" ? <IoMoon size={20} /> : <LuSun size={20} />}
+          </Button>
         </HStack>
       </Flex>
     </Container>
@@ -35,3 +51,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+//
